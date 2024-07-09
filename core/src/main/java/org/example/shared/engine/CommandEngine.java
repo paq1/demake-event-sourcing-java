c61f694b67710f2cafecce91696d31846acc6b48
paq1 <p.paquin1016@laposte.net>
2024-07-09 08:50:22 +0200

    feat: jdk21

--- core/src/main/java/org/example/shared/engine/CommandEngine.java
+++ core/src/main/java/org/example/shared/engine/CommandEngine.java
@@ -1,10 +1,17 @@
 package org.example.shared.engine;
 
 public class CommandEngine<STATES, COMMANDS, EVENTS> {
-    // todo construire le store
-    // todo construire le journal
-    // todo construire le command dispatcher
+    // todo private store
+    // todo private journal
+    // todo private command dispatcher
+    // todo private reducer
+
+    public CommandEngine(
+            // todo passer les repos, engine, reducer
+    ) {
+
+    }
 
 
-    // todo def une methode permettant de "compute" le l'event
+    // todo def une methode permettant de "compute" (sauvegarde en db)
 }
